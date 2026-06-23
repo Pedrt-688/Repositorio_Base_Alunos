5389ba6d7ef4ac2986910a208179e547b13bea77
Pedrt-688 <pedrohenriquesilvabispo10@gmail.com>
2026-06-23 11:35:29 -0300

    Add files via upload

---
+++ Modulo 2/Exercicios/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Cristiano RH - Contrata mais que o bobo do Messi")
+### colocar imagem
+st.sidebar.image("logo.png")
+###Acima imagem
+nome = st.text_input('Digite o nome do Funcionário: ')
+idade= st.text_input('Digite a idade do Funcionário: ')
+email= st.text_input('Digite o Email do Funcionário: ')
+salario= st.text_input('Digite o Salario do Funcionário: ')
+cargo= st.text_input('Digite o Cargo do funcionário: ')
